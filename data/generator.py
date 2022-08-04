0b53689c018677d7dda7fb09ef1e93012da6c8d0
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-08-04 16:40:59 -0500

    update

--- data/generator.py
+++ data/generator.py
@@ -2,21 +2,22 @@ import numpy as np
 from torch import nn
 
 
-def generate(m, n, sigma, params, size):
+def generate(m, n, coeffs, params, size):
     """ Generate data for non-linear ICA
     :param m: dimension of the latent variable
     :param n: dimension of the target variable
-    :param sigma: standard deviation of the target variable
+    :param coeffs: coefficients for data generation
     :param params: parameters for the non-linearity
     :param size: number of samples
     :return: z and x
     """
 
+    sigma = coeffs["sigma"]
     z = np.empty(shape=(0, m))
     x = np.empty(shape=(0, n))
     for _ in range(size):
         z_ = np.random.multivariate_normal(mean=np.zeros(m), cov=np.eye(m)).reshape(1, -1)
-        f_ = nonlinearity(z, m, n, params)
+        f_ = nonlinearity(z, coeffs, params)
         x_ = np.random.multivariate_normal(mean=f_, cov=(sigma**2)*np.eye(n)).reshape(1, -1)
         z = np.concatenate([z, z_], axis=0)
         x = np.concatenate([x, x_], axis=0)
@@ -24,23 +25,18 @@ def generate(m, n, sigma, params, size):
     return z, x
 
 
-def nonlinearity(z, m, n, params):
+def nonlinearity(z, coeffs, params):
     """ Non-linear function for nonlinear ICA
     :param z: latent vector
-    :param m: dimension of the latent variable
-    :param n: dimension of the target variable
+    :param coeffs: coefficients for data generation
     :param params: parameters for the non-linearity
-    :return:
+    :return: output from non-linearity
     """
 
-    # get activation function
+    # generate non-linearity
     activation = params["activation"]
     activation_func = getattr(nn, activation)
-
-    # generate non-linearity
-    np.random.seed(10)
-    w = np.random.rand(n, m)
-    b = np.random.rand(n)
+    w, b = coeffs["w"], coeffs["b"]
     f = activation_func(w @ z + b)
 
     return f
--- params/params.py
+++ params/params.py
@@ -1,5 +1,10 @@
+import numpy as np
+np.random.seed(10)
 n = 1
 m = 1
-sigma = 3
 
+sigma = 3
+w = np.random.rand(n, m)
+b = np.random.rand(n)
+coeffs = {"sigma": sigma, "w": w, "b": b}
 params = {"activation": "Sigmoid"}  # ["ReLU", "Sigmoid", "Tanh"]
