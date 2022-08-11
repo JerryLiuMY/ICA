eabdb32059b0a2fb27b7cd587a5b2685c70c8cf5
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-08-11 18:33:48 -0500

    Update latent.py

--- visualization/latent.py
+++ visualization/latent.py
@@ -13,11 +13,14 @@ def plot_latent_2d(n):
     """
 
     # initialize figure
-    fig, axes = plt.subplots(1, 4, figsize=(10, 5))
+    fig, axes = plt.subplots(1, 4, figsize=(16, 4))
     activations = ["ReLU", "Sigmoid", "Tanh", "GELU"]
     for ax, activation in zip(axes, activations):
         model_path = os.path.join(VAE_PATH, f"m2_n{n}_{activation}")
         recon_df = pd.read_csv(os.path.join(model_path, "recon_df.csv"))
         simu_df = pd.read_csv(os.path.join(model_path, "simu_df.csv"))
-        sns.kdeplot(data=recon_df, x="mu0", y="mu1", fill=True, ax=ax)
-        sns.kdeplot(data=simu_df, x="z0", y="z1", fill=True, ax=ax)
+        sns.kdeplot(data=simu_df, x="z0", y="z1", fill=True, alpha=.5, label="true", ax=ax)
+        sns.kdeplot(data=recon_df, x="mu0", y="mu1", fill=True, alpha=.5, label="recon", ax=ax)
+        ax.set_xlabel("z0")
+        ax.set_ylabel("z1")
+        ax.legend(loc="upper right")
