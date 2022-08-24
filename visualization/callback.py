9b200ee61af9d8fc488b442d2f532a8e9777e001
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-08-24 13:05:14 -0500

    Update callback.py

--- visualization/callback.py
+++ visualization/callback.py
@@ -14,16 +14,15 @@ def plot_callback(loss, llh):
     train_llh, valid_llh = llh
 
     fig, ax = plt.subplots(1, 1, figsize=(8, 4))
+    ax_ = ax.twinx()
     ax.plot(train_loss, color=sns.color_palette()[0], label="train_loss")
     ax.plot(valid_loss, color=sns.color_palette()[1], label="valid_loss")
-    ax.set_xlabel("Epoch")
-    ax.set_ylabel("ELBO")
-    ax.legend(loc="upper left")
-
-    ax_ = ax.twinx()
     ax_.plot(train_llh, color=sns.color_palette()[2], label="train_llh")
     ax_.plot(valid_llh, color=sns.color_palette()[3], label="valid_llh")
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel("ELBO")
     ax_.set_ylabel("Log-Likelihood")
+    ax.legend(loc="upper left")
     ax_.legend(loc="upper right")
 
     return fig
