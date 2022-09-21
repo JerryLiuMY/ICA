c27a1c2bdac4a4aea2beb050889a9d69fe9075a7
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-09-20 23:22:40 -0500

    Update: Add function for plotting scaling
    Add function for plotting scaling of Procrustes disparity and CCA correlation with m and n

--- run.py
+++ run.py
@@ -1,26 +1,26 @@
-from main import run_experiments
-from main import run_experiment_multi
 from global_settings import PATH_DICT
 from global_settings import get_dir
+from main import run_experiment_multi
+from main import run_experiments
 from params.params import num_trials
 from params.params import get_li_m_n
 from params.params import get_iter_m_n
 import os
 
 
-# if __name__ == "__main__":
-#     m, n = 2, 10
-#     model_name = "mleauto"
-#     exp_path = get_dir(PATH_DICT[model_name])
-#
-#     run_experiment_multi(m, n, model_name=model_name, exp_path=exp_path, train_s2=False, decoder_dgp=True)
-
-
 if __name__ == "__main__":
+    m, n = 1, 2
     model_name = "vae"
-    m_n_li, m_li_n = get_li_m_n(model_name)
-    iter_m_n = get_iter_m_n(m_n_li, m_li_n)
+    exp_path = get_dir(PATH_DICT[model_name])
 
-    for trial in range(num_trials):
-        exp_path = get_dir(os.path.join(PATH_DICT[f"{model_name}_exp"], f"trial_{trial}"))
-        run_experiments(iter_m_n, model_name=model_name, exp_path=exp_path, train_s2=False, decoder_dgp=True)
+    run_experiment_multi(m, n, model_name=model_name, exp_path=exp_path, train_s2=False, decoder_dgp=True)
+
+
+# if __name__ == "__main__":
+#     model_name = "vae"
+#     m_n_li, m_li_n = get_li_m_n(model_name)
+#     iter_m_n = get_iter_m_n(m_n_li, m_li_n)
+#
+#     for trial in range(num_trials):
+#         exp_path = get_dir(os.path.join(PATH_DICT[f"{model_name}_exp"], f"trial_{trial}"))
+#         run_experiments(iter_m_n, model_name=model_name, exp_path=exp_path, train_s2=False, decoder_dgp=True)
---
+++ visualization/scaling.py
@@ -0,0 +1,92 @@
+from params.params import get_li_m_n
+from utils.tools import activation2name
+import matplotlib.pyplot as plt
+from torch import nn
+import numpy as np
+import seaborn as sns
+import itertools
+import json
+import os
+sns.set()
+
+
+def plot_scaling(exp_path):
+    """ Plot the scaling of the disparity score & correlation with m and
+    :param exp_path: path of experiments
+    :return:
+    """
+
+    # define m_n_iter & m_iter_n
+    m_n_li, m_li_n = get_li_m_n("vae")
+    m_n_iter = sorted(list(itertools.product(*m_n_li)))
+    m_iter_n = sorted(list(itertools.product(*m_li_n)))
+
+    # generate figures
+    fig, axes = plt.subplots(2, 2, figsize=(14, 8))
+
+    disp_dict, corr_dict = build_metrics_dict(m_n_iter, exp_path)
+    title = f"n [Fixing m={[_[0] for _ in m_n_iter][0]}]"
+    xlabel, xticklabels = "Observed dimension n", [_[1] for _ in m_n_iter]
+    plot_scaling_ax(disp_dict, ax=axes[0, 0], ax_info=[title, xlabel, xticklabels, "Procrustes disparity"])
+    plot_scaling_ax(corr_dict, ax=axes[0, 1], ax_info=[title, xlabel, xticklabels, "CCA correlation"])
+
+    disp_dict, corr_dict = build_metrics_dict(m_iter_n, exp_path)
+    title = f"m [Fixing n={[_[1] for _ in m_iter_n][0]}]"
+    xlabel, xticklabels = "Latent dimension m", [_[0] for _ in m_iter_n]
+    plot_scaling_ax(disp_dict, ax=axes[1, 0], ax_info=[title, xlabel, xticklabels, "Procrustes disparity"])
+    plot_scaling_ax(corr_dict, ax=axes[1, 1], ax_info=[title, xlabel, xticklabels, "CCA correlation"])
+
+    plt.tight_layout()
+
+    return fig
+
+
+def plot_scaling_ax(metric_dict, ax, ax_info):
+    """ Plot each individual image for plot_scaling
+    :param metric_dict: dictionary of disparity
+    :param ax: axis for plotting
+    :param ax_info: information for axis
+    :return:
+    """
+
+    # define parameters
+    title, xlabel, xticklabels, metric = ax_info
+    activation_li = [nn.ReLU(), nn.Sigmoid(), nn.Tanh(), nn.LeakyReLU()]
+    activation_name_li = [activation2name(activation) for activation in activation_li]
+
+    # make plot for ax
+    ax.set_title(f"Scaling of {metric} with {title}")
+    for activation_name in activation_name_li:
+        ax.plot(metric_dict[activation_name], label=activation_name)
+
+    ax.set_xticks(np.arange(len(xticklabels)))
+    ax.set_xticklabels(xticklabels)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(metric)
+    ax.legend(loc="lower right")
+
+
+def build_metrics_dict(iterable, exp_path):
+    """ Plotting function for each individual axis in plot_scaling
+    :param iterable: iterator of m, n pairs
+    :param exp_path: path of experiments
+    :return:
+    """
+
+    # define activations
+    activation_li = [nn.ReLU(), nn.Sigmoid(), nn.Tanh(), nn.LeakyReLU()]
+    activation_name_li = [activation2name(activation) for activation in activation_li]
+
+    # build dictionary of disp & corr
+    disp_dict = {activation_name: [] for activation_name in activation_name_li}
+    corr_dict = {activation_name: [] for activation_name in activation_name_li}
+
+    for m, n in iterable:
+        log_path = os.path.join(exp_path, f"m{m}_n{n}_log")
+        with open(os.path.join(log_path, "metrics.json"), "r") as handle:
+            metrics = json.load(handle)
+        for activation_name in activation_name_li:
+            disp_dict[activation_name].append(metrics[activation_name][0])
+            corr_dict[activation_name].append(metrics[activation_name][1])
+
+    return disp_dict, corr_dict
