0532cb6d7c3566753306f7b4cab7e07843297a42
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-09-20 23:28:34 -0500

    Update: fix multiprocessing error
    Add pool.close() and pool.join() to fix error that could arise from synchrony during multiprocessing

--- main.py
+++ main.py
@@ -49,6 +49,8 @@ def run_experiment_multi(m, n, model_name, exp_path, train_s2, decoder_dgp, llh_
     iterable = [(m, n, activation) for activation in activation_li]
     with Pool(processes=4) as pool:
         pool.starmap(experiment_func, iterable=iterable)
+        pool.close()
+        pool.join()
 
     run_summary(m, n, model_name=model_name, exp_path=exp_path, llh_method=llh_method)
 
